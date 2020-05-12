d35d90dc61a24750ada0e189e4262fe67ae04417
heyx <1064042411@qq.com>
2020-05-12 17:59:10 +0800

    init project

---
+++ src/main/java/com/dwfs/shunyi/ShunyiApplication.java
@@ -0,0 +1,13 @@
+package com.dwfs.shunyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShunyiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShunyiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dwfs/shunyi/ShunyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwfs.shunyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShunyiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
